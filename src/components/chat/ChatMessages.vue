e5dd10f32c24e995488bbcad39761043ca4e601f
Yahir <yahir.eduardo.bravo@gmail.com>
2026-04-16 02:45:12 -0500

    feat: scroll on update mesage

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,16 +1,36 @@
 <script setup lang="ts">
 import type { ChatMessage } from '@/interfaces/chat-message.interface'
 import ChatBubble from './ChatBubble.vue'
+import { nextTick, useTemplateRef, watch } from 'vue'
 
 interface Props {
   messages: ChatMessage[]
 }
 
-defineProps<Props>()
+const { messages } = defineProps<Props>()
+
+// const chatRef = ref<HTMLDivElement | null>(null)
+
+const chatRef = useTemplateRef<HTMLDivElement>('chatRef')
+
+watch(
+  () => messages,
+  () => {
+    nextTick(() => {
+      if (chatRef.value) {
+        chatRef.value.scrollTo({
+          top: chatRef.value.scrollHeight,
+          behavior: 'smooth',
+        })
+      }
+    })
+  },
+  { deep: true },
+)
 </script>
 
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div class="flex-1 overflow-y-auto p-4" ref="chatRef">
     <div class="flex flex-col space-y-2">
       <!-- Messages go here -->
 
