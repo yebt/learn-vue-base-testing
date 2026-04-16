cdb420c89d64d6043dc845e8bd3631adfefd56fb
Yahir <yahir.eduardo.bravo@gmail.com>
2026-04-15 23:03:57 -0700

    feat: allow catch a new message

--- src/views/IndecisionsView.vue
+++ src/views/IndecisionsView.vue
@@ -3,11 +3,22 @@ import ChatMessages from '@/components/chat/ChatMessages.vue'
 import type { ChatMessage } from '@/interfaces/chat-message.interface'
 import { ref } from 'vue'
 import { v4 as uuidv4 } from 'uuid'
+import MessageBox from '@/components/chat/MessageBox.vue'
 
 const messages = ref<ChatMessage[]>([
   { id: uuidv4(), text: 'Hola amor, ¿cómo estás?', isSentByUser: true },
   { id: uuidv4(), text: 'Nope', isSentByUser: false},
 ])
+
+const handleMessageSend = (messageText: string) => {
+  if (messageText.trim() === '') return
+  const newMessageToSave: ChatMessage = {
+    id: uuidv4(),
+    text: messageText,
+    isSentByUser: true,
+  }
+  messages.value.push(newMessageToSave)
+}
 </script>
 
 <!-- Fuente: https://tailwindcomponents.com/component/chat-layout -->
@@ -21,5 +32,6 @@ const messages = ref<ChatMessage[]>([
     <ChatMessages :messages="messages" />
 
     <!-- Message box -->
+    <MessageBox @send-message="handleMessageSend" />
   </div>
 </template>
