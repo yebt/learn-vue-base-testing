9f8fc5a469b8f439a8bbc23c15b8697ff0d78b6b
Yahir <yahir.eduardo.bravo@gmail.com>
2026-04-15 22:35:02 -0500

    feat: work with base chat

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import './assets/styles.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
---
+++ src/views/IndecisionsView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ChatMessages from '@/components/chat/ChatMessages.vue';
+import MessageBox from '@/components/chat/MessageBox.vue';
+
+</script>
+
+<!-- Fuente: https://tailwindcomponents.com/component/chat-layout -->
+<template>
+  <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
+    <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+      <span>Mi esposa</span>
+    </div>
+
+    <!-- chat messages -->
+    <ChatMessages />
+
+    <!-- Message box -->
+    <MessageBox />
+  </div>
+</template>
