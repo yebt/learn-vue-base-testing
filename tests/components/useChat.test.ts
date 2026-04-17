e6549dfe303abdf43e5ad22c83a6bda74f3bacf7
Yahir <yahir.eduardo.bravo@gmail.com>
2026-04-17 01:54:31 -0500

    feat: replace the test with fetch of real api by fetch with mock api

--- tests/components/useChat.test.ts
+++ tests/components/useChat.test.ts
@@ -26,7 +26,41 @@ describe('useChat Composable', () => {
     expect(messages.value.length).toBe(0)
   })
 
-  it('Get messsage of API if text containt ?', async () => {
+  // it('Get messsage of API if text containt ?', async () => {
+  //   const testText = 'Do you want some coffee?'
+  //   const { messages, handleMessageSend } = useChat()
+  //
+  //   await handleMessageSend(testText)
+  //
+  //   const [myMessage, responseMessage] = messages.value
+  //
+  //   expect(myMessage).toEqual({
+  //     id: expect.any(String),
+  //     text: testText,
+  //     isSentByUser: true,
+  //   })
+  //
+  //   expect(responseMessage).toEqual({
+  //     id: expect.any(String),
+  //     text: expect.any(String),
+  //     isSentByUser: false,
+  //     image: expect.any(String),
+  //   })
+  //
+  //   // validate URL
+  //   const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i
+  //   expect(urlPattern.test(responseMessage?.image ?? '')).toBe(true)
+  // })
+
+  it('mock response - fetch API', async () => {
+    const mockResponse = {
+      answer: 'yes',
+      image: 'https://example.com/image.jpg',
+    }
+    window.fetch = vi.fn().mockResolvedValue({
+      json: () => Promise.resolve(mockResponse),
+    })
+
     const testText = 'Do you want some coffee?'
     const { messages, handleMessageSend } = useChat()
 
@@ -34,22 +68,21 @@ describe('useChat Composable', () => {
 
     const [myMessage, responseMessage] = messages.value
 
+    expect(responseMessage).toEqual({
+      id: expect.any(String),
+      text: mockResponse.answer,
+      isSentByUser: false,
+      image: mockResponse.image,
+    })
+
     expect(myMessage).toEqual({
       id: expect.any(String),
       text: testText,
       isSentByUser: true,
     })
 
-    expect(responseMessage).toEqual({
-      id: expect.any(String),
-      text: expect.any(String),
-      isSentByUser: false,
-      image: expect.any(String),
-    })
-
     // validate URL
     const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i
     expect(urlPattern.test(responseMessage?.image ?? '')).toBe(true)
-
   })
 })
