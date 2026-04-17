1dc0e308b476d77d7730b5418c5753aff79f7184
Yahir <yahir.eduardo.bravo@gmail.com>
2026-04-16 22:30:24 -0500

    feat: add example of unit testing and script to run test no watch

--- tests/helper/sum.test.ts
+++ tests/helper/sum.test.ts
@@ -1,18 +1,31 @@
-import { test, expect } from "vitest";
-import { sum } from "../../src/helper/sum.ts";
+import { test, expect, describe } from 'vitest'
+import { sum, sumArray } from '../../src/helper/sum.ts'
 
-test("sum of 1 and 2 should be 3", () => {
+test('sum of 1 and 2 should be 3', () => {
   // expect(sum(1, 2)).toBe(3);
 
   // Arrage
-  const a = 1;
-  const b = 2;
+  const a = 1
+  const b = 2
 
   // Act
-  const result = sum(a, b);
+  const result = sum(a, b)
 
   // Assert
   // expect(result).toBe(3);// More flexible
-  expect(result).toBe(a + b);// More flexible
+  expect(result).toBe(a + b) // More flexible
+})
 
-});
+describe('Sum Array', () => {
+  test('Sum in Array be 0 on empty array', () => {
+    expect(sumArray([])).toBe(0)
+  })
+
+  test('Sum in Array be operate array of numbers', () => {
+    const arr = [1, 2, 3, 4]
+
+    const result = sumArray(arr)
+
+    expect(result).toBe(10)
+  })
+})
